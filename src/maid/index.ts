6e95850f7f105b2a15ca86a75e175b4bd4d1e340
Darwin Gautalius <darwin@datasaur.ai>
2020-06-07 12:21:51 +0700

    expose toggle

--- src/maid/index.ts
+++ src/maid/index.ts
@@ -1,3 +1,6 @@
 export * from './factories/createContextWithHook';
 export * from './factories/createContextualPortal';
+export * from './hooks/useCounter';
+export * from './hooks/useLocalStorage';
 export * from './hooks/useRefresh';
+export * from './hooks/useToggle';
